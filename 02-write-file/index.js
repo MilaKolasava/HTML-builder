fe39fa6efda2b6acd83539b4f85ac6ffcaf4c63c
Mila Kolasava <kolasava.dev@gmail.com>
2023-05-07 14:53:05 +0200

    feat: add solution for task 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+const { stdin, stdout } = process;
+
+const filePath = path.join(__dirname, 'text.txt');
+const writeStream = fs.createWriteStream(filePath);
+
+function writeToStream(data) {
+    writeStream.write(data);
+}
+
+stdin.setEncoding('utf8');
+
+stdout.write('Введите текст (для завершения введите "exit" или Ctrl + C):\n');
+
+stdin.on('data', (data) => {
+    if (data.trim().toLowerCase() === 'exit') {
+        process.exit();
+    }
+    writeToStream(data);
+});
+
+process.on('exit', () => stdout.write('Текст записан'));
